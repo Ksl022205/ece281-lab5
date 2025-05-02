11a96c4c816dcf4169be8d27f09f827c1c7e67e5
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-05-02 15:16:56 -0600

    Update ALU.vhd

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -4,9 +4,9 @@ library ieee;
 
 entity ALU is
     port (
-        i_op1     : in  std_logic_vector(7 downto 0);
-        i_op2     : in  std_logic_vector(7 downto 0);
-        i_opcode  : in  std_logic_vector(2 downto 0);
+        i_A       : in  std_logic_vector(7 downto 0);
+        i_B       : in  std_logic_vector(7 downto 0);
+        i_op      : in  std_logic_vector(2 downto 0);
         o_result  : out std_logic_vector(7 downto 0);
         o_flags   : out std_logic_vector(3 downto 0)  -- Zero, Negative, Overflow, Carry
     );
@@ -21,24 +21,24 @@ architecture behavioral of ALU is
 
 begin
     -- ALU operations
-    alu_proc : process(i_op1, i_op2, i_opcode)
-        variable v_op1     : signed(8 downto 0);
-        variable v_op2     : signed(8 downto 0);
+    alu_proc : process(i_A, i_B, i_op)
+        variable v_A       : signed(8 downto 0);
+        variable v_B       : signed(8 downto 0);
         variable v_result  : signed(8 downto 0);
     begin
         -- Sign-extend inputs
-        v_op1 := signed(i_op1(7) & i_op1);
-        v_op2 := signed(i_op2(7) & i_op2);
+        v_A := signed(i_A(7) & i_A);
+        v_B := signed(i_B(7) & i_B);
 
-        case i_opcode is
+        case i_op is
             when "000" =>  -- ADD
-                v_result := v_op1 + v_op2;
+                v_result := v_A + v_B;
             when "001" =>  -- SUB
-                v_result := v_op1 - v_op2;
+                v_result := v_A - v_B;
             when "010" =>  -- AND
-                v_result := signed('0' & (i_op1 and i_op2));
+                v_result := signed('0' & (i_A and i_B));
             when "011" =>  -- OR
-                v_result := signed('0' & (i_op1 or i_op2));
+                v_result := signed('0' & (i_A or i_B));
             when others =>
                 v_result := (others => '0');
         end case;
@@ -57,8 +57,8 @@ begin
         c_negative <= v_result(7);
 
         -- Overflow flag for add/sub
-        if i_opcode = "000" or i_opcode = "001" then
-            c_overflow <= (i_op1(7) xnor i_op2(7)) and (i_op1(7) xor v_result(7));
+        if i_op = "000" or i_op = "001" then
+            c_overflow <= (i_A(7) xnor i_B(7)) and (i_A(7) xor v_result(7));
         else
             c_overflow <= '0';
         end if;
