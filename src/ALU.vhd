32b49974fb6b3a43e5f72829e9e3373522370309
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-05-04 22:27:52 -0600

    Update ALU.vhd

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -14,47 +14,47 @@ end ALU;
 
 architecture behavioral of ALU is
     component ripple_adder is 
-    Port( A : in STD_LOGIC_VECTOR(3 downto 0);
-          B : in std_logic_vector(3 downto 0);
+    Port( A : in STD_LOGIC_VECTOR (3 downto 0);
+          B : in STD_LOGIC_VECTOR (3 downto 0);
           Cin : in STD_LOGIC;
           S : out STD_LOGIC_VECTOR (3 downto 0);
-          Cout : out std_logic
+          Cout : out STD_LOGIC
           );
     end component ripple_adder;
     
-    signal X_low, X_high    :   std_logic_vector(3 downto 0);
-    signal Y_low, Y_high    :   std_logic_vector(3 downto 0);
-    signal med              :   std_logic_vector(7 downto 0);
+    signal A_low, A_high    :   std_logic_vector(3 downto 0);
+    signal B_low, B_high    :   std_logic_vector(3 downto 0);
+    signal B_mod            :   std_logic_vector(7 downto 0);
     signal sum_low, sum_high :  std_logic_vector(3 downto 0);
     signal carry_low : STD_LOGIC;
     signal carry_high : STD_LOGIC;
     signal alu_result : STD_LOGIC_VECTOR(7 downto 0);
-    signal Z_in       : STD_LOGIC;
+    signal Cin       : STD_LOGIC;
     signal sum_final  : STD_LOGIC_VECTOR(7 downto 0);
     signal xnor_s     : std_logic;
     signal xor_s      : std_logic;
     signal alu_not    : std_logic;
     signal x_and      : std_logic;
 begin 
-    X_high <= i_A(7 downto 4);
-    X_low  <= i_A(3 downto 0);
-    med <= i_B when i_op /= "001" else (not i_B);
-    Y_high <= med(7 downto 4);
-    Y_low <= med(3 downto 0);
-    Z_in <= '1' when i_op = "001" else '0';
+    A_high <= i_A(7 downto 4);
+    A_low  <= i_A(3 downto 0);
+    B_mod <= i_B when i_op /= "001" else (not i_B);
+    B_high <= B_mod(7 downto 4);
+    B_low <= B_mod(3 downto 0);
+    Cin <= '1' when i_op = "001" else '0';
     
     ripple_adder_1: ripple_adder
         port map(
-            A => X_low,
-            B => Y_low,
-            Cin => Z_in,
+            A => A_low,
+            B => B_low,
+            Cin => Cin,
             S => sum_low,
-            Cout => carry_high
+            Cout => carry_low
             );
      ripple_adder_2: ripple_adder
          port map(
-            A => X_high,
-            B => Y_high,
+            A => A_high,
+            B => B_high,
             Cin => carry_low,
             S => sum_high,
             Cout => carry_high
@@ -66,8 +66,8 @@ begin
      with i_op select
      alu_result <= sum_final when "000",
                    sum_final when "001",
-                   (med and i_A) when "010",
-                   (med or i_A) when "011",
+                   (B_mod and i_A) when "010",
+                   (B_mod or i_A) when "011",
                    (others => '0') when others;
               
       o_result <= alu_result;
